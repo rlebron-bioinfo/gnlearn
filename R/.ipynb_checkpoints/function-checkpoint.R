44cd12968b55099045ad213558502fb22ac5f5b4
Ricardo Lebrón <rlebron@go.ugr.es>
2020-05-16 13:07:26 +0200

    Just playing with roxygen2

---
+++ R/.ipynb_checkpoints/function-checkpoint.R
@@ -0,0 +1,14 @@
+#' A Test Function
+#'
+#' This function allows you to test your package.
+#' @param message Say something.
+#' @keywords test message
+#' @export
+#' @examples
+#' test.function()
+#' test.function('The way to get started is to quit talking and begin doing.')
+#' test.function(message='Your time is limited, so don't waste it living someone else's life.')
+
+test.function <- function(message='hello world!') {
+    print(message)
+}
