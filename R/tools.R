0d7f7b906a43e1ce9b37f3af7b992a8b64e21a6a
Ricardo Lebrón <rlebron@go.ugr.es>
2020-06-02 14:05:42 +0200

    Graphs Comparison - horizontal/vertical plots option

--- R/tools.R
+++ R/tools.R
@@ -449,6 +449,7 @@ third.axis <- function(layout) {
 #' @param max.steps Maximum number of steps in the path during marginalization. Default: Inf
 #' @param arcs Whether or not to list the arcs. Default: FALSE.
 #' @param plot Whether or not to plot the differences between the two graphs. Default: TRUE
+#' @param vertical.plot If you draw the comparison plots horizontally. Otherwise, they will be drawn horizontally. Default: FALSE
 #' @keywords graph comparison
 #' @export
 #' @examples
@@ -456,7 +457,7 @@ third.axis <- function(layout) {
 #' comparison <- compare.graphs(obj1, obj2, plot=FALSE)
 
 compare.graphs <- function(learned, true, marginalize=c('none','learned','true','both'),
-                           max.steps=Inf, arcs=FALSE, plot=TRUE) {
+                           max.steps=Inf, arcs=FALSE, plot=TRUE, vertical.plot=FALSE) {
     marginalize <- match.arg(marginalize)
     learned <- as.igraph(learned)
     true <- as.igraph(true)
@@ -508,6 +509,12 @@ compare.graphs <- function(learned, true, marginalize=c('none','learned','true',
 
         igraph::E(true.x)$color <- rgb(0.0,0.0,0.7,0.9)
         igraph::E(true.x)$lty <- 'dashed'
+
+        if (vertical.plot) {
+            par(mfrow = c(3, 1), mar = c(2, 2, 2, 2))
+        } else {
+            par(mfrow = c(1, 3), mar = c(2, 2, 2, 2))
+        }
 
         igraph::plot.igraph(tp,
             main='True Positive Arches',
